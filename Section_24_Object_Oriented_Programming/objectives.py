6f481bbd03463a9c66e5acae3901676f793ca424
George Wolf <gwolf204@gmail.com>
2021-05-02 15:21:29 -0400

    uploading to move to PC

---
+++ Section_24_Object_Oriented_Programming/objectives.py
@@ -0,0 +1,6 @@
+### Object Oriented Programming (OOP) ###
+
+# Define what Object Oriented Programming is
+# Understand encapsulation and abstraction
+# Create classes and instances and attach methods and properties to each
+
